3e8da2693b6a2f1a28dd5361b1520f929ee63615
juju <132360412+jumduarte@users.noreply.github.com>
2024-04-30 16:47:50 -0300

    grss

---
+++ src/app/index.js
@@ -0,0 +1,34 @@
+import { View, Image, Text } from 'react-native';
+import { styles } from '../css/style';
+import Botao from '../components/botao';
+import InputTexto from '../components/inputTexto';
+import { useState } from 'react';
+
+export default function App() {
+  const [valida, setValida] = useState();
+  const atualizaTexto = (newText) => {
+    console.log(newText);
+  }
+
+
+  return (
+    <View style={styles.container}>
+      <Image
+        style={{ width: 300, height: 80, marginBottom: 30 }}
+        resizeMode="contain"
+        source={require('../../assets/facebook.png')}
+      />
+      <InputTexto dica="Número de celular ou email" />
+      <InputTexto dica= "senha" seguranca={true}/>
+
+      <Botao btn="entrar" cor='#0000FF' src='home' />
+      <View style={styles.cont}>
+        <View style={styles.line} />
+        <Text style={styles.text} >ou</Text>
+        <View style={styles.line}></View>
+      </View>
+      <Botao btn="criar nova conta" src='excluido' />
+
+    </View>
+  );
+}
---
+++ src/css/botaoStyle.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    button: {
+     paddingVertical: 20,
+     paddingHorizontal: 40,
+     borderRadius: 5,
+     marginVertical: 10,
+     borderWidth:1,
+     borderColor:'59606B'
+    },
+    buttonPressed: {
+        backgroundColor: '#0056b3'
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 16,
+        textAlign: 'center',
+    }
+  });
+  
